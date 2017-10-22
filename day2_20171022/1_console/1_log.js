f74252a1b9b401021b6753cf953167f3219f756f
thu <mingfei.net@gmail.com>
2017-10-22 14:56:15 +0800

    Initial commit

---
+++ day2_20171022/1_console/1_log.js
@@ -0,0 +1,5 @@
+let data = {id:3, count:36};
+console.log(`count is %d`, data.count);
+console.log(data);
+
+// log > info > warn > error > fetal
---
+++ day2_20171022/1_global.js
@@ -0,0 +1,4 @@
+// window
+// window.document.write('...');
+
+// global
