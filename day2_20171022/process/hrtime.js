83cce913423dcb693164885c0082e0ce645b962d
thu <mingfei.net@gmail.com>
2017-10-22 15:34:48 +0800

    Update

--- day2_20171022/process/hrtime.js
+++ day2_20171022/process/hrtime.js
@@ -5,10 +5,15 @@ console.log(process.hrtime());
 // nano-second 1ns = 10^-9s
 
 let start = process.hrtime(); //
-for(let i = 0; i < 10000; i++) {
+for (let i = 0; i < 10000; i++) {
     // ...
 }
 // let end = process.hrtime(); //
 let diff = process.hrtime(start);
 
 console.log(diff); // difference
+
+let begin = process.hrtime();
+setTimeout(() => {
+    console.log('diff: ' + process.hrtime(begin));
+}, 1000);
