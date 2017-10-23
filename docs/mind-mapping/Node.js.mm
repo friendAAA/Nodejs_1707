f52a666dcfb9eb7757d0447c2651851e1ae3b5c6
thu <mingfei.net@gmail.com>
2017-10-23 10:02:57 +0800

    Update

--- docs/mind-mapping/Node.js.mm
+++ docs/mind-mapping/Node.js.mm
@@ -2,8 +2,8 @@
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node CREATED="1508404948198" ID="ID_268066897" MODIFIED="1508722189971" TEXT="Node.js">
 <font NAME="Consolas" SIZE="12"/>
-<node CREATED="1508721330036" FOLDED="true" ID="ID_73419346" MODIFIED="1508722185896" POSITION="left" TEXT="API">
-<node CREATED="1508405007450" FOLDED="true" HGAP="18" ID="ID_788103558" MODIFIED="1508722181536" TEXT="Globals">
+<node CREATED="1508721330036" ID="ID_73419346" MODIFIED="1508723636487" POSITION="left" TEXT="API">
+<node CREATED="1508405007450" HGAP="18" ID="ID_788103558" MODIFIED="1508723670689" TEXT="Globals">
 <font NAME="Consolas" SIZE="12"/>
 <node CREATED="1508405157285" ID="ID_1643502463" MODIFIED="1508721610939" TEXT="console object">
 <font NAME="Consolas" SIZE="12"/>
@@ -21,7 +21,7 @@
 <node CREATED="1508405879277" ID="ID_31827307" MODIFIED="1508405881361" TEXT="console.warn([data][, ...args])"/>
 </node>
 </node>
-<node CREATED="1508405184269" FOLDED="true" ID="ID_156133102" MODIFIED="1508722179714" TEXT="process object">
+<node CREATED="1508405184269" ID="ID_156133102" MODIFIED="1508723679067" TEXT="process object">
 <font NAME="Consolas" SIZE="12"/>
 <node CREATED="1508465432673" ID="ID_688602695" MODIFIED="1508721640824" TEXT="Process Events">
 <font NAME="Consolas" SIZE="12"/>
@@ -204,7 +204,7 @@
 <font NAME="Consolas" SIZE="12"/>
 </node>
 </node>
-<node CREATED="1508405224681" FOLDED="true" ID="ID_1389475432" MODIFIED="1508721661674" TEXT="Timers">
+<node CREATED="1508405224681" ID="ID_1389475432" MODIFIED="1508723673384" TEXT="Timers">
 <font NAME="Consolas" SIZE="12"/>
 <node CREATED="1508405246426" ID="ID_451296633" MODIFIED="1508487566073" TEXT="Immediate class">
 <font NAME="Consolas" SIZE="12"/>
@@ -248,250 +248,6 @@
 <node CREATED="1508720940476" ID="ID_1998388832" MODIFIED="1508720949227" TEXT="Readline"/>
 <node CREATED="1508721015698" ID="ID_157050817" MODIFIED="1508721020599" TEXT="Query Strings"/>
 <node CREATED="1508721034091" ID="ID_1698296018" MODIFIED="1508721036559" TEXT="URL"/>
-<node CREATED="1508405007450" FOLDED="true" HGAP="18" ID="ID_1734049501" MODIFIED="1508721336616" TEXT="Globals">
-<font NAME="Consolas" SIZE="12"/>
-<node CREATED="1508405075478" ID="ID_1987145938" MODIFIED="1508721294455" TEXT="Global Objects">
-<font NAME="Consolas" SIZE="12"/>
-<node CREATED="1508405157285" FOLDED="true" ID="ID_1969571388" MODIFIED="1508721199000" TEXT="console object">
-<font NAME="Consolas" SIZE="12"/>
-<node CREATED="1508405712973" FOLDED="true" ID="ID_883244171" MODIFIED="1508487566102" TEXT="Class console">
-<font NAME="Consolas" SIZE="12"/>
-<node CREATED="1508405747297" ID="ID_812686710" MODIFIED="1508405793671" TEXT="new Console(stdout[,stderr])"/>
-<node CREATED="1508405819730" ID="ID_977298442" MODIFIED="1508405821738" TEXT="console.assert(value[, message][, ...args])"/>
-<node CREATED="1508405831100" ID="ID_1339505178" MODIFIED="1508405834173" TEXT="console.dir(obj[, options])"/>
-<node CREATED="1508405841489" ID="ID_451287204" MODIFIED="1508405843731" TEXT="console.error([data][, ...args])"/>
-<node CREATED="1508405849178" ID="ID_1680626666" MODIFIED="1508405850343" TEXT="console.info([data][, ...args])"/>
-<node CREATED="1508405856175" ID="ID_550830316" MODIFIED="1508405857207" TEXT="console.log([data][, ...args])"/>
-<node CREATED="1508405861953" ID="ID_164075875" MODIFIED="1508405862695" TEXT="console.time(label)"/>
-<node CREATED="1508405866809" ID="ID_1895645172" MODIFIED="1508405867711" TEXT="console.timeEnd(label)"/>
-<node CREATED="1508405872611" ID="ID_1005113059" MODIFIED="1508405873281" TEXT="console.trace(message[, ...args])"/>
-<node CREATED="1508405879277" ID="ID_705343673" MODIFIED="1508405881361" TEXT="console.warn([data][, ...args])"/>
-</node>
-</node>
-<node CREATED="1508405184269" FOLDED="true" ID="ID_492629195" MODIFIED="1508721197371" TEXT="process object">
-<font NAME="Consolas" SIZE="12"/>
-<node CREATED="1508465432673" ID="ID_801819429" MODIFIED="1508487566102" TEXT="Process Events">
-<font NAME="Consolas" SIZE="12"/>
-<node CREATED="1508466558788" ID="ID_1168403218" MODIFIED="1508487566101" TEXT="Event: &apos;beforeExit&apos;">
-<font NAME="Consolas" SIZE="12"/>
-</node>
-<node CREATED="1508466591506" ID="ID_220138487" MODIFIED="1508487566101" TEXT="Event: &apos;disconnect&apos;">
-<font NAME="Consolas" SIZE="12"/>
-</node>
-<node CREATED="1508466602986" ID="ID_1700207834" MODIFIED="1508487566101" TEXT="Event: &apos;exit&apos;">
-<font NAME="Consolas" SIZE="12"/>
-</node>
-<node CREATED="1508466610575" ID="ID_1966376792" MODIFIED="1508487566101" TEXT="Event: &apos;message&apos;">
-<font NAME="Consolas" SIZE="12"/>
-</node>
-<node CREATED="1508466622930" ID="ID_1195192173" MODIFIED="1508487566101" TEXT="Event: &apos;rejectionHandled&apos;">
-<font NAME="Consolas" SIZE="12"/>
-</node>
-<node CREATED="1508466653346" ID="ID_1807880871" MODIFIED="1508487566101" TEXT="Event: &apos;unCaughtException&apos;">
-<font NAME="Consolas" SIZE="12"/>
-<node CREATED="1508466730297" ID="ID_664727669" MODIFIED="1508487566101" TEXT="Warning: Using &apos;unCaughtException&apos; correctly">
-<font NAME="Consolas" SIZE="12"/>
-</node>
-</node>
-<node CREATED="1508466683297" ID="ID_1521472567" MODIFIED="1508487566101" TEXT="Event: &apos;unHandledException&apos;">
-<font NAME="Consolas" SIZE="12"/>
-</node>
-<node CREATED="1508466696510" ID="ID_747059840" MODIFIED="1508487566099" TEXT="Event: &apos;warning&apos;">
-<font NAME="Consolas" SIZE="12"/>
-<node CREATED="1508466774317" ID="ID_586511716" MODIFIED="1508487566099" TEXT="Emiting custom warnings">
-<font NAME="Consolas" SIZE="12"/>
-</node>
-<node CREATED="1508466803386" ID="ID_1661562209" MODIFIED="1508487566099" TEXT="Emitting custom deprecation warnings">
-<font NAME="Consolas" SIZE="12"/>
-</node>
-</node>
-<node CREATED="1508466704803" ID="ID_132370430" MODIFIED="1508487566099" TEXT="Signal Events">
-<font NAME="Consolas" SIZE="12"/>
-</node>
-</node>
-<node CREATED="1508465456120" ID="ID_37671152" MODIFIED="1508487566099" TEXT="process.abort()">
-<font NAME="Consolas" SIZE="12"/>
-</node>
-<node CREATED="1508465475774" ID="ID_1976223342" MODIFIED="1508487566099" TEXT="process.arch">
-<font NAME="Consolas" SIZE="12"/>
-</node>
-<node CREATED="1508465483101" ID="ID_405889794" MODIFIED="1508487566099" TEXT="process.argv">
-<font NAME="Consolas" SIZE="12"/>
-</node>
-<node CREATED="1508465490670" ID="ID_1699855138" MODIFIED="1508487566099" TEXT="process.argv()">
-<font NAME="Consolas" SIZE="12"/>
-</node>
-<node CREATED="1508465515823" ID="ID_1218108816" MODIFIED="1508487566099" TEXT="process.chdir(directory)">
-<font NAME="Consolas" SIZE="12"/>
-</node>
-<node CREATED="1508465540253" ID="ID_1483786359" MODIFIED="1508487566099" TEXT="process.config">
-<font NAME="Consolas" SIZE="12"/>
-</node>
-<node CREATED="1508465528670" ID="ID_1779089172" MODIFIED="1508487566099" TEXT="process.connected">
-<font NAME="Consolas" SIZE="12"/>
-</node>
-<node CREATED="1508465581166" ID="ID_405226120" MODIFIED="1508487566094" TEXT="process.cpuUsage([previousValue])">
-<font NAME="Consolas" SIZE="12"/>
-</node>
-<node CREATED="1508465602997" ID="ID_1087200846" MODIFIED="1508487566094" TEXT="process.cwd()">
-<font NAME="Consolas" SIZE="12"/>
-</node>
-<node CREATED="1508465622836" ID="ID_589457808" MODIFIED="1508487566094" TEXT="process.disconnect()">
-<font NAME="Consolas" SIZE="12"/>
-</node>
-<node CREATED="1508466276711" ID="ID_992879907" MODIFIED="1508487566094" TEXT="process.env">
-<font NAME="Consolas" SIZE="12"/>
-</node>
-<node CREATED="1508466292276" ID="ID_553319751" MODIFIED="1508487566092" TEXT="process.emitWarning(warning[,name][,ctor])">
-<font NAME="Consolas" SIZE="12"/>
-<node CREATED="1508466340187" ID="ID_1309829151" MODIFIED="1508487566090" TEXT="Avoding duplicate warnings">
-<font NAME="Consolas" SIZE="12"/>
-</node>
-</node>
-<node CREATED="1508466376557" ID="ID_706027624" MODIFIED="1508487566090" TEXT="process.execArgv">
-<font NAME="Consolas" SIZE="12"/>
-</node>
-<node CREATED="1508466384169" ID="ID_1858405311" MODIFIED="1508487566088" TEXT="process.execPath">
-<font NAME="Consolas" SIZE="12"/>
-</node>
-<node CREATED="1508466408865" ID="ID_562407398" MODIFIED="1508487566088" TEXT="process.exit([code])">
-<font NAME="Consolas" SIZE="12"/>
-</node>
-<node CREATED="1508466423262" ID="ID_1388259780" MODIFIED="1508487566088" TEXT="process.exitCode">
-<font NAME="Consolas" SIZE="12"/>
-</node>
-<node CREATED="1508466439625" ID="ID_1827172357" MODIFIED="1508487566088" TEXT="process.getegid()">
-<font NAME="Consolas" SIZE="12"/>
-</node>
-<node CREATED="1508466453852" ID="ID_1882586830" MODIFIED="1508487566088" TEXT="process.geteuid()">
-<font NAME="Consolas" SIZE="12"/>
-</node>
-<node CREATED="1508466463941" ID="ID_1048450833" MODIFIED="1508487566088" TEXT="process.getgid()">
-<font NAME="Consolas" SIZE="12"/>
-</node>
-<node CREATED="1508466473467" ID="ID_648774974" MODIFIED="1508487566088" TEXT="process.getgroups()">
-<font NAME="Consolas" SIZE="12"/>
-</node>
-<node CREATED="1508466483158" ID="ID_643370826" MODIFIED="1508487566088" TEXT="process.getuid()">
-<font NAME="Consolas" SIZE="12"/>
-</node>
-<node CREATED="1508466497238" ID="ID_1502655237" MODIFIED="1508487566088" TEXT="process.hrtime([time])">
-<font NAME="Consolas" SIZE="12"/>
-</node>
-<node CREATED="1508469437549" ID="ID_1148428362" MODIFIED="1508487566086" TEXT="process.initgroups(user,extra_group)">
-<font NAME="Consolas" SIZE="12"/>
-</node>
-<node CREATED="1508469465005" ID="ID_1723953537" MODIFIED="1508487566086" TEXT="process.kill(pid[,signal])">
-<font NAME="Consolas" SIZE="12"/>
-</node>
-<node CREATED="1508469482755" ID="ID_557569462" MODIFIED="1508487566085" TEXT="process.mainModule">
-<font NAME="Consolas" SIZE="12"/>
-</node>
-<node CREATED="1508469493788" ID="ID_205704918" MODIFIED="1508487566083" TEXT="process.memoryUsage()">
-<font NAME="Consolas" SIZE="12"/>
-</node>
-<node CREATED="1508469513743" ID="ID_1926093163" MODIFIED="1508487566083" TEXT="process.nextTick(callback[,...args])">
-<font NAME="Consolas" SIZE="12"/>
-</node>
-<node CREATED="1508469542751" ID="ID_1081530395" MODIFIED="1508487566083" TEXT="process.pid">
-<font NAME="Consolas" SIZE="12"/>
-</node>
-<node CREATED="1508469548330" ID="ID_377796814" MODIFIED="1508487566083" TEXT="process.platform">
-<font NAME="Consolas" SIZE="12"/>
-</node>
-<node CREATED="1508469554864" ID="ID_507175926" MODIFIED="1508487566083" TEXT="process.release">
-<font NAME="Consolas" SIZE="12"/>
-</node>
-<node CREATED="1508469604974" ID="ID_85140621" MODIFIED="1508487566081" TEXT="process.send(message[,sendHandle[,options][,callback])">
-<font NAME="Consolas" SIZE="12"/>
-</node>
-<node CREATED="1508469673058" ID="ID_319250911" MODIFIED="1508487566081" TEXT="process.setegid(id)">
-<font NAME="Consolas" SIZE="12"/>
-</node>
-<node CREATED="1508469684733" ID="ID_1626907336" MODIFIED="1508487566081" TEXT="process.seteuid(id)">
-<font NAME="Consolas" SIZE="12"/>
-</node>
-<node CREATED="1508469700039" ID="ID_259449340" MODIFIED="1508487566081" TEXT="process.setgid(id)">
-<font NAME="Consolas" SIZE="12"/>
-</node>
-<node CREATED="1508469707630" ID="ID_1040554075" MODIFIED="1508487566081" TEXT="process.setgroups(groups)">
-<font NAME="Consolas" SIZE="12"/>
-</node>
-<node CREATED="1508469721907" ID="ID_87335127" MODIFIED="1508487566081" TEXT="process.setuid(id)">
-<font NAME="Consolas" SIZE="12"/>
-</node>
-<node CREATED="1508469732725" ID="ID_1058891677" MODIFIED="1508487566081" TEXT="process.stderr">
-<font NAME="Consolas" SIZE="12"/>
-</node>
-<node CREATED="1508469737509" ID="ID_1601228103" MODIFIED="1508487566081" TEXT="process.stdin">
-<font NAME="Consolas" SIZE="12"/>
-</node>
-<node CREATED="1508469747325" ID="ID_922969615" MODIFIED="1508487566081" TEXT="process.stdout">
-<font NAME="Consolas" SIZE="12"/>
-<node CREATED="1508469762192" ID="ID_1260491309" MODIFIED="1508487566077" TEXT="A note on process I/O">
-<font NAME="Consolas" SIZE="12"/>
-</node>
-</node>
-<node CREATED="1508469783218" ID="ID_1895027773" MODIFIED="1508487566077" TEXT="process.title">
-<font NAME="Consolas" SIZE="12"/>
-</node>
-<node CREATED="1508469792421" ID="ID_1036037523" MODIFIED="1508487566077" TEXT="process.umask([mask])">
-<font NAME="Consolas" SIZE="12"/>
-</node>
-<node CREATED="1508469820904" ID="ID_358869632" MODIFIED="1508487566077" TEXT="process.uptime()">
-<font NAME="Consolas" SIZE="12"/>
-</node>
-<node CREATED="1508469834489" ID="ID_852934823" MODIFIED="1508487566077" TEXT="process.version">
-<font NAME="Consolas" SIZE="12"/>
-</node>
-<node CREATED="1508469841044" ID="ID_27209197" MODIFIED="1508487566077" TEXT="process.versions">
-<font NAME="Consolas" SIZE="12"/>
-</node>
-<node CREATED="1508469850402" ID="ID_597796768" MODIFIED="1508487566073" TEXT="Exit Codes">
-<font NAME="Consolas" SIZE="12"/>
-</node>
-</node>
-<node CREATED="1508405224681" FOLDED="true" ID="ID_325450770" MODIFIED="1508721200112" TEXT="timers">
-<font NAME="Consolas" SIZE="12"/>
-<node CREATED="1508405246426" ID="ID_1033272651" MODIFIED="1508487566073" TEXT="Immediate class">
-<font NAME="Consolas" SIZE="12"/>
-</node>
-<node CREATED="1508405268432" ID="ID_339440742" MODIFIED="1508487566073" TEXT="Timeout class">
-<font NAME="Consolas" SIZE="12"/>
-<node CREATED="1508483602646" ID="ID_335633148" MODIFIED="1508487566073" TEXT="timeout.ref()">
-<font NAME="Consolas" SIZE="12"/>
-</node>
-<node CREATED="1508483617842" ID="ID_690022444" MODIFIED="1508487566073" TEXT="timeout.unref()">
-<font NAME="Consolas" SIZE="12"/>
-</node>
-</node>
-<node CREATED="1508405287998" ID="ID_804568865" MODIFIED="1508487566073" TEXT="Scheduling Timers">
-<font NAME="Consolas" SIZE="12"/>
-<node CREATED="1508483637783" ID="ID_1802719321" MODIFIED="1508487566073" TEXT="setImmediate(callback[,...args])">
-<font NAME="Consolas" SIZE="12"/>
-</node>
-<node CREATED="1508483663191" ID="ID_4068614" MODIFIED="1508487566073" TEXT="setInterval(callback,delay[,...args])">
-<font NAME="Consolas" SIZE="12"/>
-</node>
-<node CREATED="1508483684585" ID="ID_852020098" MODIFIED="1508487566073" TEXT="setTimeout(callback,delay[,...args])">
-<font NAME="Consolas" SIZE="12"/>
-</node>
-</node>
-<node CREATED="1508405311188" ID="ID_1597479257" MODIFIED="1508487566073" TEXT="Cancelling Timers">
-<font NAME="Consolas" SIZE="12"/>
-<node CREATED="1508483718485" ID="ID_1963600789" MODIFIED="1508487566073" TEXT="clearImmediate(immediate)">
-<font NAME="Consolas" SIZE="12"/>
-</node>
-<node CREATED="1508483731019" ID="ID_134143040" MODIFIED="1508487566073" TEXT="clearInterval(interval)">
-<font NAME="Consolas" SIZE="12"/>
-</node>
-<node CREATED="1508483738677" ID="ID_797602003" MODIFIED="1508487566072" TEXT="clearTimeout(timeout)">
-<font NAME="Consolas" SIZE="12"/>
-</node>
-</node>
-</node>
-</node>
-</node>
 <node CREATED="1508720916061" ID="ID_1542998578" MODIFIED="1508720919914" TEXT="OS"/>
 <node CREATED="1508720940476" ID="ID_652454039" MODIFIED="1508720949227" TEXT="Readline"/>
 <node CREATED="1508721015698" ID="ID_1581294401" MODIFIED="1508721020599" TEXT="Query Strings"/>
