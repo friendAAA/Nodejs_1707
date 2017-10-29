8b2d9536e24768cc6cf369bb23e287a1cde2b4b0
thu <mingfei.net@gmail.com>
2017-10-29 11:41:20 +0800

    Update

--- day3_20171029/readline/2_file-line.js
+++ day3_20171029/readline/2_file-line.js
@@ -1,3 +1,11 @@
 const fs = require('fs');
 const readline = require('readline');
 
+let file = console.log(process.argv[1]);
+readline.createInterface({
+    input: fs.createReadStream('question.js'),
+    output: process.stdout,
+    terminal: true
+}).on('line', (line) => {
+    console.log(line);
+});
