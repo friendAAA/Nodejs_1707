e0570b73264ba3ccc8e795a64bfe7dbbd927f5a1
thu <mingfei.net@gmail.com>
2017-10-29 11:09:02 +0800

    Update

--- day3_20171029/2_tmpdir.js
+++ day3_20171029/2_tmpdir.js
@@ -12,5 +12,7 @@ console.log(os.freemem());
 console.log(os.cpus());
 console.log(os.networkInterfaces());
 
-console.log(os.constants.signals.SIGINT); // Ctrl + Shift + U
-console.log(os.constants.errno.EADDRINUSE); // Ctrl + Shift + U
+console.log(os.constants.signals.SIGINT);
+// Ctrl + Shift + U
+console.log(os.constants.errno.EADDRINUSE);
+// Ctrl + Shift + U
